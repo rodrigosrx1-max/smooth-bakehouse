3d639275d4a8e1f646ef6afa47b7482a1b34271a
rodrigosrx1-max <rodrigo.srx1@gmail.com>
2026-02-18 00:13:48 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "smooth-cache-v12";
+const CACHE_NAME = "smooth-cache-v13";
 
 const FILES_TO_CACHE = [
   "/",
@@ -80,4 +80,5 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
